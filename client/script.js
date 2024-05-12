c8d2c48c9de1c0a1d042e14a5f68d0232ea3eaa7
mizael-anthony <mizaelanthony07@gmail.com>
2024-05-12 08:31:39 +0300

    chore: clean up

--- client/script.js
+++ client/script.js
@@ -1,6 +1,22 @@
-const inputNomPrenoms = document.querySelector('#nom_prenoms')
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import '@geoman-io/leaflet-geoman-free';  
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';   
 
 
-inputNomPrenoms.addEventListener('keyup', () => {
-    console.log("mande")
-})
+const madagascar = [-18.917, 46.802]
+const zoom = 13
+const map = L.map('map').setView(madagascar, zoom)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+// add Leaflet-Geoman controls with some options to the map  
+map.pm.addControls({  
+  position: 'topleft',  
+  drawCircle: false,  
+});  
