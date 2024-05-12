90127ad23616db6c2c4e6675112c1f8e6ab9510a
mizael-anthony <mizaelanthony07@gmail.com>
2024-05-12 09:23:09 +0300

    chore: setup cdn map

--- client/map.js
+++ client/map.js
@@ -1,9 +1,3 @@
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import '@geoman-io/leaflet-geoman-free';  
-import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';   
-
-
 const madagascar = [-18.917, 46.802]
 const zoom = 13
 const map = L.map('map').setView(madagascar, zoom)
